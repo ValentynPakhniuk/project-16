6fa38e86b3c6215e8cfffdd0e8a7d96e9571d112
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-03-01 19:47:14 +0200

    deleted files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import flatpickr from 'flatpickr';
-// Додатковий імпорт стилів
-import 'flatpickr/dist/flatpickr.min.css';
-// import { includes } from 'lodash';
-import NewsApiService from './js/NewsApiService';
-
-import './js/change-theme';
