33f8768b13688ea739d60ed236dc640cc2da0f88
Katerina Urizko <k.urizko@gmail.com>
2023-03-02 17:04:04 +0200

    Update change-theme.js

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -1,10 +1,34 @@
-(() => {
-  const tgl = document.querySelector('.switch__theme');
-  tgl.addEventListener('click', e => {
-    if (e.target.classList.contains('theme-light')) {
-      document.body.classList.remove('dark-mode');
-    } else if (e.target.classList.contains('theme-dark')) {
-      document.body.classList.add('dark-mode');
-    }
-  });
-})();
+// (() => {
+//   const tgl = document.querySelector('.switch__theme');
+//   tgl.addEventListener('click', e => {
+//     if (e.target.classList.contains('theme-light')) {
+//       document.body.classList.remove('dark-mode');
+//     } else if (e.target.classList.contains('theme-dark')) {
+//       document.body.classList.add('dark-mode');
+//     }
+//   });
+// })();
+
+// mode switcher with Local Storage
+
+const tgl = document.querySelector('.switch__theme');
+tgl.addEventListener('click', onSwitchMode);
+
+addClassToBody();
+
+function onSwitchMode() {
+  if (localStorage.getItem('theme') === 'dark-mode') {
+    localStorage.removeItem('theme');
+  } else {
+    localStorage.setItem('theme', 'dark-mode');
+  }
+  addClassToBody();
+}
+
+function addClassToBody() {
+  if (localStorage.getItem('theme') === 'dark-mode') {
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+  }
+}
