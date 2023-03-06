0401dadfc47bf0c6657b6a2dd2208d640789f6a9
Vitalii Protsko <provitaleks@gmail.com>
2023-03-06 17:07:33 +0200

    finish weather

--- src/js/weather.js
+++ src/js/weather.js
@@ -8,12 +8,53 @@ const refs = {
   currentYear: document.querySelector('#current-year'),
   buttonSwitch: document.querySelector('#switch'),
   weatherDaily: document.querySelector('#weather-daily'),
-  weatherWeek: document.querySelector('#wether-week'),
+  weatherWeek: document.querySelector('#weather-week'),
 }
 
 const APIURL = 'https://api.openweathermap.org/data/2.5/';
 const APIKEY = 'ea8d3210fb1031e19e4ed7c502a65955';
 const date = new Date();
+let lat = '40.7142700';
+let lon = '-74.0059700';
+
+function weatherRequest() {
+  
+  try {
+    fetch(`${APIURL}weather?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=metric`)
+      .then(response => response.json())
+      .then(weather => {
+          refs.temperature.textContent = Math.round(weather.main.temp);
+          refs.weatherConditions.textContent = weather.weather[0].main;
+          refs.cityName.textContent = weather.name;
+  });
+  } catch (error) {
+    console.log(error);
+  }
+  
+  try {
+    fetch(`${APIURL}forecast?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=metric`)
+      .then(response => response.json())
+      .then(weatherWeek => {
+          refs.cityNameWeek.textContent = weatherWeek.city.name;
+
+          for (let i = 0; i < weatherWeek.list.length; i += 8) {
+            const weatherDay = weatherWeek.list[i];
+            const p = {
+              date: weatherDay.dt_txt.split(' ')[0],
+              minTemperature: weatherDay.main.temp_min,
+              maxTemperature: weatherDay.main.temp_max,
+              weatherParameter: weatherDay.weather[0].main,
+            }
+            
+            refs.weatherWeek.insertAdjacentHTML('beforeEnd', `
+            <div class="wether-week__date">${p.date}</div>
+            <div class="wether-week">${p.minTemperature}&deg - ${p.maxTemperature}&deg ${p.weatherParameter}</div>`);
+          }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const options = {
   enableHighAccuracy: true,
@@ -24,29 +65,16 @@ const options = {
 function success(pos) {
   const crd = pos.coords;
 
-  fetch(`${APIURL}weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=${APIKEY}&units=metric`)
-  .then(response => response.json())
-  .then(weather => {
-    refs.temperature.textContent = Math.round(weather.main.temp);
-    refs.weatherConditions.textContent = weather.weather[0].main;
-    refs.cityName.textContent = weather.name;
-  });
+lat = crd.latitude;
+lon = crd.longitude;
 
-  fetch(`${APIURL}forecast?lat=${crd.latitude}&lon=${crd.longitude}&cnt=60&appid=${APIKEY}`)
-  .then(response => response.json())
-  .then(weatherWeek => {
-  refs.cityNameWeek.textContent = weatherWeek.city.name;
-
-//   if (weatherWeek.list) {
-    
-//   }
-
-//   console.log(weatherWeek);
-});
+  weatherRequest();
 };
 
 function error(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
+
+  weatherRequest();
 };
 
 navigator.geolocation.getCurrentPosition(success, error, options); 
