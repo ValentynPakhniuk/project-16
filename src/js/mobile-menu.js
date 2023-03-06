f99c7ea89e801e9cd07ee94c1d49f7d82ac21c1f
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-03-06 19:02:47 +0200

    update mobile-menu.js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,11 +2,14 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
+  const searchForm = document.querySelector('#search-form');
   const body = document.body;
+
   menuBtnRef.addEventListener('click', () => {
     const expanded =
       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
     menuBtnRef.classList.toggle('is-open');
+    searchForm.classList.toggle('visually-hidden');
     menuBtnRef.getAttribute('aria-expanded', !expanded);
     mobileMenuRef.classList.toggle('is-open');
     body.classList.toggle('modal-open');
