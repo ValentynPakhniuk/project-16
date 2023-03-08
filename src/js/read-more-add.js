9d48d1d01f3b1f3109fd8126327163da4524a1d0
IvanDeltaplan <ivannikolaev777@gmail.com>
2023-03-08 19:02:54 +0200

    read
    updated

--- src/js/read-more-add.js
+++ src/js/read-more-add.js
@@ -1,122 +1,122 @@
-// import {
-//     STORAGE_KEY_READ,
-//     VISUALLY_HIDDEN_CLASS,
+import {
+    STORAGE_KEY_READ,
+    VISUALLY_HIDDEN_CLASS,
     
-//     ADD_READ_BTN,
-//     REMOTE_READ_BTN,
-//     ADD_FAVORITE_BTN,
-//     LIST_CARD_SELECTOR,
-//   } from './constants';
+    ADD_READ_BTN,
+    REMOTE_READ_BTN,
+    ADD_FAVORITE_BTN,
+    LIST_CARD_SELECTOR,
+  } from './constants';
   
-//   const FAVORITE_PAGE_PATH = '/read.html';
+  const FAVORITE_PAGE_PATH = '/read.html';
   
-//   const newsList = document.querySelector(LIST_CARD_SELECTOR);
-//   newsList.addEventListener('click', saveReadNews);
-//   //newsList.addEventListener('click', removeFavorite);
-//   let idFrom=[]; 
-//   function saveReadNews(e) {
-//     //e.preventDefault();
-//     if (e.target.classList.contains(ADD_READ_BTN)) {
+  const newsList = document.querySelector(LIST_CARD_SELECTOR);
+  newsList.addEventListener('click', saveReadNews);
+  //newsList.addEventListener('click', removeFavorite);
+  let idFrom=[]; 
+  function saveReadNews(e) {
+    //e.preventDefault();
+    if (e.target.classList.contains(ADD_READ_BTN)) {
 
 
 
 
 
-//       const elementBlockPhoto2 = e.target.parentElement;
+      const elementBlockPhoto2 = e.target.parentElement;
 
-//       const elementCard = elementBlockPhoto2.parentElement;
-//       const elementBlockPhoto = elementCard.children[0]
-//       //console.log(elementCard)
-//       console.log(elementBlockPhoto)
+      const elementCard = elementBlockPhoto2.parentElement;
+      const elementBlockPhoto = elementCard.children[0]
+      //console.log(elementCard)
+      console.log(elementBlockPhoto)
   
-//       const readObj = {};
-//       readObj.imgUrl = elementBlockPhoto.children[0].src;
+      const readObj = {};
+      readObj.imgUrl = elementBlockPhoto.children[0].src;
       
-//       readObj.category = elementBlockPhoto.children[1].innerText;
-//       readObj.title = elementCard.children[1].innerText;
-//       readObj.text = elementCard.children[2].innerText;
-//       readObj.date = elementCard.children[3].children[0].innerText;
-//       readObj.readMoreLink = elementCard.children[3].children[1].href;
-//       readObj.id = elementCard.id;
-//     //    for (let i=0; i++; i<10) {
-//                         // if(parsedNews[0].id != readObj.id){    
+      readObj.category = elementBlockPhoto.children[1].innerText;
+      readObj.title = elementCard.children[1].innerText;
+      readObj.text = elementCard.children[2].innerText;
+      readObj.date = elementCard.children[3].children[0].innerText;
+      readObj.readMoreLink = elementCard.children[3].children[1].href;
+      readObj.id = elementCard.id;
+    //    for (let i=0; i++; i<10) {
+                        // if(parsedNews[0].id != readObj.id){    
                 
                         
                         
 
-//       const savedApiData2 =
-//         JSON.parse(localStorage.getItem(STORAGE_KEY_READ)) || [];
-//       savedApiData2.push(readObj);
+      const savedApiData2 =
+        JSON.parse(localStorage.getItem(STORAGE_KEY_READ)) || [];
+      savedApiData2.push(readObj);
   
-//       localStorage.setItem(STORAGE_KEY_READ, JSON.stringify(savedApiData2));
+      localStorage.setItem(STORAGE_KEY_READ, JSON.stringify(savedApiData2));
                         
                        
   
-//     //   // приховуємо "add to favorite", включаємо 'Remove from favorite'
-//     //   e.target.classList.add(VISUALLY_HIDDEN_CLASS);
-//      //  elementBlockPhoto.children[4].classList.remove(VISUALLY_HIDDEN_CLASS);
-//     }
+    //   // приховуємо "add to favorite", включаємо 'Remove from favorite'
+    //   e.target.classList.add(VISUALLY_HIDDEN_CLASS);
+     //  elementBlockPhoto.children[4].classList.remove(VISUALLY_HIDDEN_CLASS);
+    }
 
-//   }
-//   const parsedNews = JSON.parse(localStorage.getItem(STORAGE_KEY_READ)) || [];
+  }
+  const parsedNews = JSON.parse(localStorage.getItem(STORAGE_KEY_READ)) || [];
 
-//   parsedNews.forEach(element => {
-//     console.log(element.id)
+  parsedNews.forEach(element => {
+    console.log(element.id)
     
-//   });
-//     console.log(parsedNews)
+  });
+    console.log(parsedNews)
 
 
-// //console.log(idFrom)
+//console.log(idFrom)
 
-// //   function removeFavorite(e) {
-// //     if (e.target.classList.contains(REMOTE_FAVORITE_BTN)) {
-// //       const currentLi = e.target.parentElement.parentElement;
-// //       const currentId = currentLi.id;
+//   function removeFavorite(e) {
+//     if (e.target.classList.contains(REMOTE_FAVORITE_BTN)) {
+//       const currentLi = e.target.parentElement.parentElement;
+//       const currentId = currentLi.id;
   
-// //       const savedData =
-// //         JSON.parse(localStorage.getItem(STORAGE_KEY_FAVORITE)) || [];
+//       const savedData =
+//         JSON.parse(localStorage.getItem(STORAGE_KEY_FAVORITE)) || [];
   
-// //       const newSavedData = savedData.filter(item => item.id !== currentId);
+//       const newSavedData = savedData.filter(item => item.id !== currentId);
   
-// //       localStorage.setItem(STORAGE_KEY_FAVORITE, JSON.stringify(newSavedData));
+//       localStorage.setItem(STORAGE_KEY_FAVORITE, JSON.stringify(newSavedData));
   
-// //       turnOffFavorite(currentLi);
+//       turnOffFavorite(currentLi);
   
-// //       visibleNoData();
-// //     }
-// //   }
+//       visibleNoData();
+//     }
+//   }
   
-// //   function turnOffFavorite(elem) {
-// //     if (window.location.pathname.includes(FAVORITE_PAGE_PATH)) {
-// //       elem.remove();
-// //     } else {
-// //       elem
-// //         .querySelector(`.${REMOTE_FAVORITE_BTN}`)
-// //         .classList.add(VISUALLY_HIDDEN_CLASS);
-// //       elem
-// //         .querySelector(`.${ADD_FAVORITE_BTN}`)
-// //         .classList.remove(VISUALLY_HIDDEN_CLASS);
-// //     }
-// //   }
+//   function turnOffFavorite(elem) {
+//     if (window.location.pathname.includes(FAVORITE_PAGE_PATH)) {
+//       elem.remove();
+//     } else {
+//       elem
+//         .querySelector(`.${REMOTE_FAVORITE_BTN}`)
+//         .classList.add(VISUALLY_HIDDEN_CLASS);
+//       elem
+//         .querySelector(`.${ADD_FAVORITE_BTN}`)
+//         .classList.remove(VISUALLY_HIDDEN_CLASS);
+//     }
+//   }
   
-// //   function visibleNoData() {
-// //     if (window.location.pathname.includes(READ_PAGE_PATH)) {
-// //       const savedData =
-// //         JSON.parse(localStorage.getItem(STORAGE_KEY_FAVORITE)) || [];
-// //       const noDataBlock = document.querySelector('.no-data .error');
-// //       const containerCardList = document.querySelector('.container.list__cards');
-// //       if (savedData.length == 0) {
-// //         noDataBlock.querySelector('.title-error').innerHTML =
-// //           'We haven’t found <br> favorite news';
-// //         noDataBlock.classList.remove(VISUALLY_HIDDEN_CLASS);
-// //         containerCardList.classList.add(VISUALLY_HIDDEN_CLASS);
-// //       } else {
-// //         noDataBlock.classList.add(VISUALLY_HIDDEN_CLASS);
-// //         containerCardList.classList.remove(VISUALLY_HIDDEN_CLASS);
-// //       }
-// //     }
-// //   }
+//   function visibleNoData() {
+//     if (window.location.pathname.includes(READ_PAGE_PATH)) {
+//       const savedData =
+//         JSON.parse(localStorage.getItem(STORAGE_KEY_FAVORITE)) || [];
+//       const noDataBlock = document.querySelector('.no-data .error');
+//       const containerCardList = document.querySelector('.container.list__cards');
+//       if (savedData.length == 0) {
+//         noDataBlock.querySelector('.title-error').innerHTML =
+//           'We haven’t found <br> favorite news';
+//         noDataBlock.classList.remove(VISUALLY_HIDDEN_CLASS);
+//         containerCardList.classList.add(VISUALLY_HIDDEN_CLASS);
+//       } else {
+//         noDataBlock.classList.add(VISUALLY_HIDDEN_CLASS);
+//         containerCardList.classList.remove(VISUALLY_HIDDEN_CLASS);
+//       }
+//     }
+//   }
   
-// //    visibleNoData();
+//    visibleNoData();
   
