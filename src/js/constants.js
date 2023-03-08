3f2371abdf22b600f1fc31e5b8349bb5061fc461
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-03-09 00:55:23 +0200

    fix constants.js

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,7 +1,7 @@
 export const PAGE_SIZE = 10;
 export const STORAGE_KEY_FAVORITE = 'favoriteNews';
-const date = new Date;
-dateForKey = (`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`);
+const date = new Date();
+export const dateForKey = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
 export const STORAGE_KEY_READ = dateForKey;
 export const VISUALLY_HIDDEN_CLASS = 'visually-hidden';
 export const REMOTE_FAVORITE_BTN = 'remove-favorite-btn';
@@ -9,6 +9,3 @@ export const REMOTE_READ_BTN = 'remove-read-btn';
 export const ADD_FAVORITE_BTN = 'add-favorite-btn';
 export const ADD_READ_BTN = 'card-more-news';
 export const LIST_CARD_SELECTOR = '.list-card';
-
-
-
