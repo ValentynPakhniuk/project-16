7784dc4add409f44c9909c27915c0da92da79b2f
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2023-03-10 13:03:50 +0200

    fixed

--- src/js/active-page.js
+++ src/js/active-page.js
@@ -1,12 +1,21 @@
 let activePage = window.location.pathname;
-const path = window.location.pathname.split('/');
+const path = activePage.split('/');
 if (path[path.length - 1] === '') {
-  activePage = '/index.html';
-}
-const menuNav = document.querySelector('.navigation__container');
-const menuItem = menuNav.querySelector(`[href='${activePage}']`);
-menuItem.classList.add('site-nav__link--current');
+  const menuNav = document.querySelector('.navigation__container');
+  const menuItem = menuNav.querySelector(`.nav__link.site-nav__link.link`);
+  menuItem.classList.add('site-nav__link--current');
 
-const mobMenu = document.querySelector('.site-nav__mobile');
-const mobItem = mobMenu.querySelector(`[href='${activePage}']`);
-mobItem.parentElement.classList.add('container__item--current');
+  const mobMenu = document.querySelector('.site-nav__mobile');
+  const mobItem = mobMenu.querySelector(
+    `.nav__link.site-nav__link.mobile__nav-link.link`
+  );
+  mobItem.parentElement.classList.add('container__item--current');
+} else {
+  const menuNav = document.querySelector('.navigation__container');
+  const menuItem = menuNav.querySelector(`[href='${activePage}']`);
+  menuItem.classList.add('site-nav__link--current');
+
+  const mobMenu = document.querySelector('.site-nav__mobile');
+  const mobItem = mobMenu.querySelector(`[href='${activePage}']`);
+  mobItem.parentElement.classList.add('container__item--current');
+}
